da4430983012050f897d38f2e1548e1c74672c2e
Patrik Mrnka <patrikmrnka@Patrik-MacBook-Pro.local>
2021-05-14 15:44:18 +0200

    Username condition

--- form.php
+++ form.php
@@ -1,11 +1,24 @@
 <?php 
 
     if (isset($_POST["submit"])) { // metoda _POST si vytáhla informace z name "submit", isset = je submit nastavené?
+        
+        $minimum = 3; //minimální počet znaků
+        $maximum = 10; //maximální počet znaků
+        
         $username = $_POST["username"]; //zachycení Uživatelského jména
         $password = $_POST["password"]; //zachycení Hesla
-    echo "Ahoj " . $username; // vypsání Uživatelského jména
-    echo "<br>";
-    echo "Tvé heslo je: " . $password; // vypsání Hesla
+        echo "Ahoj " . $username; // vypsání Uživatelského jména
+        echo "<br>";
+        echo "Tvé heslo je: " . $password; // vypsání Hesla
+        echo "<br>";
+
+        if (strlen($username) <= $minimum) { //podmínka pro délku jména
+            echo "Uživatelské jméno musí být delší než 3 znaky";
+        } 
+        else if (strlen($username) >= $maximum) {
+            echo "Uživatelské jméno musí být kratší než 10 znaků";
+        }
+
     }
 
 ?>
