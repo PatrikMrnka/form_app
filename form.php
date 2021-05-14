93d06788bfe86dd36fa15cc6f5541690f223cf3d
Patrik Mrnka <patrikmrnka@Patrik-MacBook-Pro.local>
2021-05-14 15:34:05 +0200

    data catch

--- form.php
+++ form.php
@@ -1,7 +1,11 @@
 <?php 
 
-    if (isset($_POST["submit"])) { // metoda _POST si vytáhla informace z name "submit"
-        echo "Formulář úspěšný";
+    if (isset($_POST["submit"])) { // metoda _POST si vytáhla informace z name "submit", isset = je submit nastavené?
+        $username = $_POST["username"]; //zachycení Uživatelského jména
+        $password = $_POST["password"]; //zachycení Hesla
+    echo $username;
+    echo "<br>";
+    echo $password;
     }
 
 ?>
@@ -16,9 +20,9 @@
 </head>
 <body>
     <form action="form.php" method="post">
-        <input type="text" placeholder="Uživatelské jméno">
+        <input type="text" name="username" placeholder="Uživatelské jméno">
         <br>
-        <input type="password" placeholder="Heslo">
+        <input type="password" name="password" placeholder="Heslo">
         <br>
         <input type="submit" name="submit" value="Odeslat">
     </form>
